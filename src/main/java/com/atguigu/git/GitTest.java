fac12d8b634f7f6358a3c659febb9a71b842f41b
ren <ren@123.com>
2022-08-20 15:58:15 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2022-08-20 15:53
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello1 !!!");
+        System.out.println("hello2 !!!");
+        System.out.println("hello3 !!!");
+    }
+}
